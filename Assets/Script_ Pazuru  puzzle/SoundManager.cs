0758dac27601ccfc1eeaa800467e407e3bb66b92
BuildTools <unconfigured@null.spigotmc.org>
2019-03-17 17:09:36 +0900

    リロードかけたときにsoundが重複する問題を修正

---
+++ Assets/Script_ Pazuru  puzzle/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject sound;
+
+    // Use this for initialization
+    void Start () {
+        if (!GameObject.Find("Sound(Clone)"))
+        {
+            Instantiate(sound);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
